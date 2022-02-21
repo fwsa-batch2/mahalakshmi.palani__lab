16481ed7bce56e2bb8692d4520afab32f6d0cbfe
mahalakshmi2000 <82790009+mahalakshmi2000@users.noreply.github.com>
2022-02-21 13:00:24 +0530

    added java file

---
+++ java/Helloworld.java
@@ -0,0 +1,17 @@
+public class Helloworld{
+public static void main(String[] args) {
+        //System.out.println("Hello world");
+    }
+}
+class OddorEven{
+    public static void main(String[] args){
+        int num = 2;
+        if( num % 2 == 0){
+            System.out.println("this number is even");
+        }
+        else{
+            System.out.println("this number is odd");
+        }
+
+    }  
+}
