6442c65d6dd96cd32ae5108433a42cfa0186b7f0
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2022-04-25 18:51:19 +0530

    added encapsulation exercise

---
+++ java/klassy/src/com/Fwsa/UserData.java
@@ -0,0 +1,36 @@
+package com.Fwsa;
+class Encapsulation{
+	private int AccNo;
+	private String UserName;
+	
+	public int getAccNo() {
+		return AccNo;
+	}
+	public void setAccNo(int AccNo) {
+		 this.AccNo = AccNo;
+	}
+	public String getName() {
+		return UserName;
+	}
+	public void setName(String UserName) {
+		 this.UserName = UserName;
+	}
+	
+}
+public class UserData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello world");
+		Encapsulation myObj = new Encapsulation();
+		myObj.setAccNo(123456);
+		myObj.setName("Mahalakshmi");
+		System.out.println("Account number = "+myObj.getAccNo());
+		System.out.println("User Name = "+myObj.getName());
+		
+		
+		
+		
+	}
+
+}
