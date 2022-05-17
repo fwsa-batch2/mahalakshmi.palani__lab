cdf28920457e3a7c5ebb26d316d6216c959d4c51
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2022-05-17 19:04:26 +0530

    added thread excercise

---
+++ java/klassy/src/com/thread/example.java
@@ -0,0 +1,28 @@
+package com.thread;
+
+public class example extends Thread{
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		Task t1 = new Task("Task1");
+		Task t2 = new Task("Task2");
+		Task t3 = new Task("Task3");
+		t1.start();
+		t2.start();
+		t3.start();
+		
+		//System.out.println("Task Finished...");
+	}
+}
+
+class Task extends Thread{  
+	String name;
+	public Task(String name) {
+		this.name = name;
+	}
+	public void run() {
+		for(int i = 0; i < 5; i++) {
+			System.out.println(name + " printing " + i);
+		}
+	}
+}
