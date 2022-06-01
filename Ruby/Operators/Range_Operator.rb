7aa8ac77f905ad4134157496527ffeab790f4eb3
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2022-06-01 14:50:46 +0530

    added Operator excercise

---
+++ Ruby/Operators/Range_Operator.rb
@@ -0,0 +1,30 @@
+puts("Range operator")
+range1 = (1...5).to_a
+puts"#{range1}"
+range2 =("a".."h").to_a
+puts"#{range2}"
+
+Myrange = 1...8
+#checking the content
+output1 = Myrange.include?(5)
+puts"#{output1}"
+
+#getting the maximum value
+output2 = Myrange.max
+puts"#{output2}"
+
+#getting the minimum value
+output3 = Myrange.min
+puts"#{output3}"
+
+#slicing the ranges
+retval = Myrange.select {|i| i < 5 }
+puts "MyRange selected: #{retval}"
+retval = Myrange.reject {|i| i < 5 }
+puts "MyRange rejected: #{retval}"
+
+#iterating over the range
+Myrange.each do|i|
+    puts i
+        
+end
