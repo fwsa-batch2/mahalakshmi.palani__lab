ad074b7bd61e7d35b494f652b04bfaace99e4f03
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2022-06-16 09:10:51 +0530

    added file read and write exercise

---
+++ Ruby/IO/writingfile.rb
@@ -0,0 +1,14 @@
+firstfile = File.new("myfile","w+")
+firstfile.syswrite("File Handling")  
+firstfile.upend("hello world")
+fileobject.close()
+
+  
+# Creating a file
+# fileobject = File.new("sample.txt", "w+"); 
+  
+# # Writing to the file
+# fileobject.syswrite("File Handling");    
+  
+#  # Closing a file
+# fileobject.close();    
