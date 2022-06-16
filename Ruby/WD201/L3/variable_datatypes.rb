26baf05a357913a13be7e6a77b51613575734221
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2022-06-16 09:21:49 +0530

    added variable and datatypes excercise

---
+++ Ruby/WD201/L3/variable_datatypes.rb
@@ -0,0 +1,22 @@
+# age = 20
+# name = "Sachin"
+# puts "I am " + name + " and I am " + age.to_s + " old."
+
+
+
+# nums = [1, 2, 3, 4, 5, 6]
+# puts "The average of these numbers are #{(nums.sum / nums.length)}"
+
+# puts 1 == 1
+# puts "this" == "that"
+# puts 1 < 2
+# puts 1 > 2
+# puts 42 >= 42
+
+puts "enter your name"
+name = gets.chomp.downcase
+puts "enter your father name"
+name2 =  gets.chomp.upcase
+puts name
+puts name2
+
