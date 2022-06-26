19100a5ab70f5e40a1d97135309cf819bf9b3ed3
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2022-06-26 17:02:09 +0530

    added java format example

---
+++ java/klassy/src/com/basics/PrintF.java
@@ -0,0 +1,13 @@
+package com.basics;
+
+public class PrintF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.printf("baeldung%nline%nterminator");
+		//System.out.printf("%B%n", false);
+		System.out.printf("%-15s%03d%n","java",100);
+		
+	}
+
+}
